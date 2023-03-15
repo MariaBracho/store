9aade53da309fd19435d42754e5dca9ba7129670
María Bracho <mariabrachovillanueva@gmail.com>
2023-03-15 18:26:32 -0400

    feat: :tada: Beging project

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  parser: "@typescript-eslint/parser",
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: "./tsconfig.json"
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/explicit-function-return-type": "off"
+  }
+};
